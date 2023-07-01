3f1a777928b4ca1b74c21d9f3beee3dbcd606e4d
Andrew <Voicedrew11@gmail.com>
2023-06-30 22:25:03 -0400

    newe changes

--- stream/convert.js
+++ stream/convert.js
@@ -1,14 +1,13 @@
-    var converter = new showdown.Converter();
+   var converter = new showdown.Converter();
 
-    fetch('journal.md')
-      .then(function(response) {
-        return response.text();
-      })
-      .then(function(markdown) {
-        var html = converter.makeHtml(markdown);
-        var journalDiv = document.getElementById("journal");
-        journalDiv.innerHTML = html;
-      })
-      .catch(function(error) {
-        console.log('Error:', error);
-      });
+fetch('https://raw.githubusercontent.com/Voicedrew11/voicedrew11.github.io/Microsoft95/stream/journal.md')
+  .then(response => response.text())
+  .then(content => {
+    // Process the content as needed
+    var html = converter.makeHtml(content);
+    var journalDiv = document.getElementById("journal");
+    journalDiv.innerHTML = html;
+  })
+  .catch(error => {
+    console.error(error);
+  });
