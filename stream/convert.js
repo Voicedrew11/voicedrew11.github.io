f12e091a12ff1392a999bc260216dd972269b616
Andrew <Voicedrew11@gmail.com>
2023-07-01 13:09:06 -0400

    Yello

--- stream/convert.js
+++ stream/convert.js
@@ -1,4 +1,7 @@
-   var converter = new showdown.Converter();
+/*
+function convertScript() {
+  return new Promise(function(resolve, reject) {
+       var converter = new showdown.Converter();
 
 fetch('https://raw.githubusercontent.com/Voicedrew11/voicedrew11.github.io/Microsoft95/stream/journal.md')
   .then(response => response.text())
@@ -11,3 +14,58 @@ fetch('https://raw.githubusercontent.com/Voicedrew11/voicedrew11.github.io/Micro
   .catch(error => {
     console.error(error);
   });
+
+    resolve();
+  });
+}
+
+function tocScript() {
+tocbot.init({
+      tocSelector: '.js-toc',
+      contentSelector: '.js-toc-content',
+      headingSelector: 'h1, h2, h3',
+      hasInnerContainers: true,
+        });
+tocbot.refresh();
+}
+
+// Usage:
+convertScript().then(function() {
+  tocScript();
+});
+*/
+
+var converter = new showdown.Converter();
+
+function convertScript(callback) {
+  fetch('https://raw.githubusercontent.com/Voicedrew11/voicedrew11.github.io/Microsoft95/stream/journal.md')
+    .then(response => response.text())
+    .then(content => {
+      // Process the content as needed
+      var html = converter.makeHtml(content);
+      var journalDiv = document.getElementById("journal");
+      journalDiv.innerHTML = html;
+
+      // Call the callback function when finished
+      callback();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function tocScript() {
+  tocbot.init({
+    tocSelector: '.js-toc',
+    contentSelector: '.js-toc-content',
+    headingSelector: 'h1, h2, h3',
+    hasInnerContainers: true,
+  });
+  tocbot.refresh();
+}
+
+// Usage:
+convertScript(function() {
+  // This anonymous function will be called when convertScript has finished executing
+  tocScript();
+});
