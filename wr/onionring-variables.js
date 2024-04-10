e74eb16cbff3fdb19499646aa5cc3c20e2e6a9ef
Andrew Priestley <voicedrew11@tutamail.com>
2024-04-09 22:05:16 -0400

    manpaint

--- wr/onionring-variables.js
+++ wr/onionring-variables.js
@@ -48,7 +48,7 @@ var sites = [
 'https://crunglechamp.neocities.org/',
 'https://alixx.ichi.city/',
 'https://omnipresence.neocities.org/',
-'https://hwii.net/',
+'https://manpaint.neocities.org/',
 ];
 
 // 'https://nhkcafe.neocities.org/',
